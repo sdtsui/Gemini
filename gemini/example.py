75284ef794225f6d86fbfa344f157dc0e6c66652
Damien Tanner <damien@tanner.me>
2017-09-14 14:27:17 +0100

    example.py now runs successfully

--- gemini/example.py
+++ gemini/example.py
@@ -1,11 +1,12 @@
 import pandas as pd
+import gemini
 import poloniex as px
 import helpers
 
 pair = "BTC_ETH"    # Use ETH pricing data on the BTC market
 period = 1800       # Use 1800 second candles
 daysBack = 30       # Grab data starting 30 days ago
-daysData = 60       # From there collect 60 days of data
+daysData = 3       # From there collect 60 days of data
 
 # Request data from Poloniex
 data = px.getPast(pair, period, daysBack, daysData)
@@ -17,30 +18,28 @@ data['date'] = pd.to_datetime(data['date'], unit='s')
 def Logic(Account, Lookback):
     try:
         # Process dataframe to collect signals
-        Lookback = helpers.getSignals(Lookback)
-        
+        # Lookback = helpers.getSignals(Lookback)
+
         # Load into period class to simplify indexing
         Lookback = helpers.Period(Lookback)
-        
+
         Today = Lookback.loc(0) # Current candle
         Yesterday = Lookback.loc(-1) # Previous candle
-        
-        if Today['signal'] == "down":
-            if Yesterday['signal'] == "down":
-                ExitPrice = Today['close']
-                for Position in Account.Positions:  
-                    if Position.Type == 'Long':
-                        Account.ClosePosition(Position, 0.5, ExitPrice)
+        print(Today)
+        if Today['close'] < Yesterday['close']:
+            ExitPrice = Today['close']
+            for Position in Account.Positions:
+                if Position.Type == 'Long':
+                    Account.ClosePosition(Position, 0.5, ExitPrice)
 
-        if Today['signal'] == "up":
-            if Yesterday['signal'] == "up":
-                Risk         = 0.03
-                EntryPrice   = Today['close']
-                EntryCapital = Account.BuyingPower*Risk
-                if EntryCapital >= 0:
-                    Account.EnterPosition('Long', EntryCapital, EntryPrice)
-     
-    except ValueError: 
+        if Today['close'] > Yesterday['close']:
+            Risk         = 0.03
+            EntryPrice   = Today['close']
+            EntryCapital = Account.BuyingPower*Risk
+            if EntryCapital >= 0:
+                Account.EnterPosition('Long', EntryCapital, EntryPrice)
+
+    except ValueError:
         pass # Handles lookback errors in beginning of dataset
 
 # Load the data into a backtesting class called Run
