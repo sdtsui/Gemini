8185a3919e7f11dcadc1efbf59a21af242a57400
Damien Tanner <damien@tanner.me>
2017-09-15 12:02:43 +0100

    18d1 is best model

--- gemini/example_cryptocompare.py
+++ gemini/example_cryptocompare.py
@@ -6,9 +6,9 @@ from datetime import *
 
 pair = ['BTC','USD']    # Use ETH pricing data on the BTC market
 daysBack = 0       # Grab data starting X days ago
-daysData = 365*5       # From there collect X days of data
-LookbackPeriod = 20 # How many days to lookback for momentum
-TradingInterval = 2 # Run trading logic every X days
+daysData = 180       # From there collect X days of data
+LookbackPeriod = 18 # How many days to lookback for momentum
+TradingInterval = 1 # Run trading logic every X days
 FeesSpread = 0.0025+0.001 # Fees 0.25% + Bid/ask spread to account for http://data.bitcoinity.org/markets/spread/6m/USD?c=e&f=m20&st=log&t=l using Kraken 0.1% as worse case
 # Request data from cryptocompare
 data = cc.getPast(pair, daysBack, daysData)
@@ -47,4 +47,4 @@ r = gemini.Run(data)
 r.Start(1000, Logic, TradingInterval, LookbackPeriod)
 
 r.Results()
-r.Chart('LookbackPeriod: {}, TradingInterval: {}'.format(LookbackPeriod,TradingInterval),ShowTrades=False)
+r.Chart('LookbackPeriod: {}, TradingInterval: {}'.format(LookbackPeriod,TradingInterval),ShowTrades=True)
